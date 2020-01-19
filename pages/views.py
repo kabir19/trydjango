2c9dcde0ef37bb77435b2f1f57d7595f6612191e
kabir19 <kashyap.wadekar321@gmail.com>
2020-01-19 16:12:48 +0530

    django templates tags and its demo

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-	return HttpResponse("<h1>Hello World!!!</h1>")
+def home_view(request, *args, **kwargs):
+	#return HttpResponse("<h1>Hello World!!!</h1>")
+	return render(request, "home.html", {})
+
+def about_view(request, *args, **kwargs):
+	my_context = {
+		"my_text": "This is about me",
+		"my_number": 1234,
+		"my_list": [123, 456, 321, "Abc"]
+	}
+	return render(request, "about.html", my_context)
+
+def contact_view(request, *args, **kwargs):
+	#return HttpResponse("<h1>Hello World!!!</h1>")
+	return render(request, "contact.html", {})
