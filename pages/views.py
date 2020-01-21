3eee752e46f66fc27962609fbf2c393595db43ce
kabir19 <kashyap.wadekar321@gmail.com>
2020-01-21 17:40:21 +0530

    Raw data save from django form to database demo.

--- pages/views.py
+++ pages/views.py
@@ -9,6 +9,7 @@ def home_view(request, *args, **kwargs):
 def about_view(request, *args, **kwargs):
 	my_context = {
 		"my_text": "This is about me",
+		"this_is_true": True,
 		"my_number": 1234,
 		"my_list": [123, 456, 321, "Abc"]
 	}
